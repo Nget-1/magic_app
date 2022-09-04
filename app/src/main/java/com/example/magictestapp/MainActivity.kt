4f81631afe9ad2d6f81bc48fa0d148463b97b33d
Ethan <ethanngkindle@gmail.com>
2022-09-03 18:16:51 -0700

    Initial setup, starting from scratch

--- app/src/main/java/com/example/magictestapp/MainActivity.kt
+++ app/src/main/java/com/example/magictestapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.magictestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val cardList = findViewById<TextView>(R.id.card_list)
+        val cardEntry = findViewById<EditText>(R.id.card_entry)
+        val addCardButton = findViewById<Button>(R.id.card_entry_button)
+
+        addCardButton.setOnClickListener{
+            val newCard = cardEntry.text.toString()
+            if(!newCard.isNullOrEmpty()){
+                cardList.text = newCard
+                cardEntry.setText("")
+            }
+        }
     }
 }
