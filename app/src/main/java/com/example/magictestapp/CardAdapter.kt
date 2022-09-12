63e3fe0b8bd48fd6128abe72867d55bd2a71b3f1
Ethan <ethanngkindle@gmail.com>
2022-09-12 10:32:23 -0700

    Update CardAdapter.kt

--- app/src/main/java/com/example/magictestapp/CardAdapter.kt
+++ app/src/main/java/com/example/magictestapp/CardAdapter.kt
@@ -4,7 +4,7 @@ import android.widget.CheckBox
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class CardAdapter : RecyclerView.Adapter<> {
+class CardAdapter : RecyclerView.Adapter<CardAdapter.ViewHolder>() {
     val cards: MutableList<Card> = mutableListOf()
 
     override fun getItemCount() = this.cards.size
@@ -17,6 +17,12 @@ class CardAdapter : RecyclerView.Adapter<> {
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         private val checkBox: CheckBox = view.findViewById(R.id.card_checkbox)
         private val cardTextTV: TextView = view.findViewById(R.id.card_list_text)
+
+        init {
+            checkBox.setOnCheckedChangeListener { button, isChecked ->
+                cards[absoluteAdapterPosition].completed = isChecked
+            }
+        }
 
         fun bind(card: Card){
             this.checkBox.isChecked = card.completed
